19c7569ebcd6cc9e7a02caacdb6d1953a1b61c2a
mart1d4 <git@mart1d4.me>
2025-11-22 04:03:45 +0100

    Finishes program. Adds bonus files. Removes test files

---
+++ get_next_line_bonus.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: git <git@mart1d4.me>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/22 03:31:39 by git               #+#    #+#             */
+/*   Updated: 2025/11/22 03:31:39 by git              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 4096
+# endif
+
+# include <fcntl.h>
+# include <stddef.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+int		ft_strlen(const char *s);
+int		ft_strlen_newline(const char *s);
+char	*ft_strndup(const char *s, int n);
+int		ft_has_newline(char *s);
+char	*ft_strjoin_and_free_old(char *s1, const char *s2);
+
+char	*get_next_line(int fd);
+
+#endif // !GET_NEXT_LINE_H
---
+++ get_next_line_utils.c
@@ -0,0 +1,92 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: git <git@mart1d4.me>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/22 03:35:42 by git               #+#    #+#             */
+/*   Updated: 2025/11/22 03:51:38 by git              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+int	ft_strlen(const char *s)
+{
+	int	counter;
+
+	counter = 0;
+	while (s && *s++)
+		counter++;
+	return (counter);
+}
+
+int	ft_strlen_newline(const char *s)
+{
+	int	len;
+
+	len = 0;
+	while (s)
+	{
+		if (*s == '\0')
+			return (len);
+		if (*s == '\n')
+			return (len + 1);
+		len++;
+		s++;
+	}
+	return (0);
+}
+
+char	*ft_strndup(const char *s, int n)
+{
+	int		i;
+	char	*str;
+
+	str = (char *)malloc(sizeof(char) * (n + 1));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (s[i] != '\0' && i < n)
+	{
+		str[i] = s[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+int	ft_has_newline(char *s)
+{
+	if (!s)
+		return (0);
+	while (*s)
+	{
+		if (*s == '\n')
+			return (1);
+		s++;
+	}
+	return (0);
+}
+
+char	*ft_strjoin_and_free_old(char *s1, const char *s2)
+{
+	int		i;
+	int		j;
+	char	*str;
+
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!str)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s1 && s1[i])
+		str[j++] = s1[i++];
+	i = 0;
+	while (s2 && s2[i])
+		str[j++] = s2[i++];
+	str[j] = 0;
+	free(s1);
+	return (str);
+}
